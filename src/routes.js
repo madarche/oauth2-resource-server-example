c4ca3179fac65d8b799b79098aff1b26c436c6d2
Marc-Aurèle DARCHE <madarche@users.noreply.github.com>
2020-11-06 20:46:47 +0100

    Use an OpenAPI schema with a validator middleware

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 'use strict'
 
+const path = require('path')
 const Router = require('@koa/router')
+const input_validation = require('openapi-validator-middleware')
 
+const config = require('./lib/config')
 const app = require('./app')
+
+const OPENAPI_SCHEMA_FILE_RPATH = 'src/openapi-schema.yml'
+const openapi_schema_file_path = path.join(config.getPkgDirPath(),
+    OPENAPI_SCHEMA_FILE_RPATH)
+input_validation.init(openapi_schema_file_path, {
+    beautifyErrors: true,
+    framework: 'koa',
+})
 
 const router = new Router()
 
@@ -12,8 +23,8 @@ router.get('/info', info.getAppInfo)
 
 const resources = require('./controller/resources')
 router
-    .get('/resources', resources.getAll)
-    .get('/resources/:resource_id', resources.get)
+    .get('/resources', input_validation.validate, resources.getAll)
+    .get('/resources/:resource_id', input_validation.validate, resources.get)
 
 app
     .use(router.routes())
